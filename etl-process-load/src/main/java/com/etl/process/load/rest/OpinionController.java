6c4b5f15c39e7244ca7afe7b4e174aae166feff5
madrimas <zapiorkowski@gmail.com>
2020-01-04 20:18:28 +0100

    imports fix

--- etl-process-load/src/main/java/com/etl/process/load/rest/OpinionController.java
+++ etl-process-load/src/main/java/com/etl/process/load/rest/OpinionController.java
@@ -3,6 +3,7 @@ package com.etl.process.load.rest;
 import com.etl.process.load.dao.OpinionRepository;
 import com.etl.process.load.entity.Opinion;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
--- etl-process-load/src/main/java/com/etl/process/load/rest/ProductController.java
+++ etl-process-load/src/main/java/com/etl/process/load/rest/ProductController.java
@@ -3,6 +3,7 @@ package com.etl.process.load.rest;
 import com.etl.process.load.dao.ProductRepository;
 import com.etl.process.load.entity.Product;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 import java.util.List;
