7585c329efa5e43b30f78efbfb843cc5b6eb12a7
Kamil <wienio96@gmail.com>
2020-01-05 14:12:42 +0100

    Javadoc Update

--- etl-process-scrapper/src/main/java/data/warehouse/EtlProcessExtractApplication.java
+++ etl-process-scrapper/src/main/java/data/warehouse/EtlProcessExtractApplication.java
@@ -3,9 +3,16 @@ package data.warehouse;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Main runner class
+ */
 @SpringBootApplication
 public class EtlProcessExtractApplication {
 
+    /**
+     * Runner class to start application
+     * @param args command line arguments for application, not required
+     */
     public static void main(String[] args) {
         SpringApplication.run(EtlProcessExtractApplication.class, args);
     }
